b6dca18512a542194a363f515b63829e7474c1ec
Maria22 <m.ushakova22@gmail.com>
2020-05-11 22:12:43 +0200

    Updates

---
+++ precache-manifest.3c96b13761c14352c4587a9e1f154200.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a55fbe21412910f829d7ba5089619f",
+    "url": "/Search-restaurants-clone-Yelp/index.html"
+  },
+  {
+    "revision": "3aaead6edd3adca1d2e0",
+    "url": "/Search-restaurants-clone-Yelp/static/css/main.21ead721.chunk.css"
+  },
+  {
+    "revision": "5b631a7ba56a5f56da04",
+    "url": "/Search-restaurants-clone-Yelp/static/js/2.70a0e2c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Search-restaurants-clone-Yelp/static/js/2.70a0e2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aaead6edd3adca1d2e0",
+    "url": "/Search-restaurants-clone-Yelp/static/js/main.9e5078cf.chunk.js"
+  },
+  {
+    "revision": "dd2a02930132acb10820",
+    "url": "/Search-restaurants-clone-Yelp/static/js/runtime-main.d6b00803.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Search-restaurants-clone-Yelp/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Search-restaurants-clone-Yelp/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
